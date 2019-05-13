fad437a69999b7e6450acc67db6e6dc253766dab
Edoardo Oranger <Edoardo>
2019-05-13 12:13:47 +0200

    Conclusa sezione per ciclo while

---
+++ while/README.md
@@ -0,0 +1,34 @@
+# STRUTTURA WHILE?
+**__NON ESISTE LA STRUTTURA WHILE IN HASKELL!!!__**
+
+Tuttavia esiste un modo alternativo per definirla, la ricorsione.
+Per ricorsione si intente una funzione, o in maniera più generale un sotto-programma, che richiama se stesso.
+
+Solitamente, per definire una chiamata ricorsiva abbiamo bisogno di alcuni elementi:
+
+1. *Una condizione di partenza che definisce fin quando si deve eseguire la ricorsione (simile alla condizione del while)*
+2. *Un caso base per cui viene definita la ricorsione*
+3. *Un caso generico per cui definire tutte le restanti iterazioni*
+
+## Notazioni
+Se in un blocco then o in un blocco else sono state definite più azioni da eseguire, e si sta utilizzando la sintassi C-like per definire il blocco, è necessario circondare le istruzioni da un blocco __do__ aggiuntivo:
+
+```
+    if (condizione)
+        then do {
+            espressione_1;
+            espressione_2;
+        }
+        else espressione_3;
+```
+
+Analogamente per la condizione __else__
+
+```
+    if (condizione)
+        then espressione_1;
+        else {
+            espressione_2;
+            espressione_3
+        }
+```
---
+++ while/while.hs
@@ -0,0 +1,42 @@
+module While where
+
+    -- Importazione delle librerie per utlizzare il metodo di stampa printf --
+    import Text.Printf
+
+    -- Definizione delle funzioni -- 
+    -- finoADiesci prende in input un intero ed esegue un'azione di Input-Output
+    finoADieci :: Integer -> IO()
+    -- incrementaDi prende in input due interi e resituisce un intero --
+    incrementaDi :: Integer -> Integer -> Integer
+    fattoriale :: Integer -> Integer
+    main :: IO()
+
+    -- Istruzioni multiple in un blocco then --
+    finoADieci numero = do {
+        if(numero <= 10)
+            then do {
+                printf "Sono al numero: %d\n" numero;
+                finoADieci (numero + 1);
+            }
+            else putStrLn ("Ho terminato di contare ...");
+        
+    }
+
+    incrementaDi numero1 numero2 = do {
+        if(numero2 > 0)
+            then incrementaDi (numero1+1) (numero2-1);
+            else numero1;
+    }
+
+    fattoriale numero = do {
+        if (numero == 0)
+            then 1
+            else numero*fattoriale (numero - 1)
+    }
+
+    main = do {
+        putStrLn "Conta fino a 10 ";
+        finoADieci 1;
+        printf "Incrementa 2 di 3: %d\n" (incrementaDi 2 3);
+        printf "Il Fattoriale di 3 è: %d\n" (fattoriale 3);
+    }
